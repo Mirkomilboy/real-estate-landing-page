0157d48084e533152fcbbbdff01177de58c4d47f
Mirkomilboy <msadirov1997@gmail.com>
2021-11-09 09:47:18 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './public/**/*.{html,js}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          blue: '#012348',
+          yellow: '#C29B40',
+        }
+      },
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif'],
+      },
+      spacing: {
+        '9px': '9px',
+        '13px': '13px',
+      },
+      maxWidth: {
+        '83rem': '83rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
